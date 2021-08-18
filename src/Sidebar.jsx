7583a9c2435e4e9dd4dc6a58f1d905e40219056f
Nishu <mainwalnishant9@gmail.com>
2021-08-18 09:42:13 +0530

    initial commit

---
+++ src/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Sidebar.css';
+
+function Sidebar() {
+    return (
+        <div>
+            <div className="sidebar">
+                <h2>I am the sidebar</h2>
+                <div className="sidebar__top">
+     <h4>Passion</h4>
+                </div>
+            </div>
+        </div>
+)}
+
+export default Sidebar;
